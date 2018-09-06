a02dfab3400f1714445250bc14f9248d5be1e319
lihuat <363533474@qq.com>
2018-09-06 13:36:10 +0800

    8月风险倒扣，已经修改区经区助单笔扣罚

---
+++ 1-区经区助扣罚免除.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import re
+"""
+在'首次M3明细/销售人员清单'中剔除工号为:25040010 犹敦信 离职
+
+"""
+
+m2 = pd.read_excel("首次M2逾期率/首次M2逾期率.xlsx",dtype={"SA工号":'O'})
+m2 = m2[['SA工号','SA姓名','首次M2单量','首次M2注册数']]
+
+people = pd.read_excel("首次M3明细/销售人员清单.xlsx",dtype={'工号':'O'})
+people = people[['工号','姓名','在岗状态','区域经理','高区经理','城市经理','角色']]
+people = people.rename(columns={'工号':'SA工号'})
+
+m2 = pd.merge(m2,people,on="SA工号",how="left",)
+m2 = m2.loc[m2["在岗状态"]=="在职"]
+
+dt1 = m2.loc[m2["角色"]=="销售员" ]
+dt2 = m2.loc[m2["角色"]=="APP功能试点,销售员"]
+
+m2 = pd.concat([dt1,dt2]).reset_index(drop=True)
+
+
+m2['区经姓名'] = 0
+for i in tqdm(range(len(m2))):
+    try:
+            m2.loc[i,'区经姓名'] = re.sub(r"团队$","",m2.loc[i,"区域经理"])
+    except:
+            pass
+
+for i in tqdm(range(len(m2))):
+     try:
+            m2.loc[i,'区经姓名']= re.findall(r'[^（）]+',m2.loc[i,'区域经理'])[1]
+     except:
+            pass
+
+m2['匹配'] = 0
+for i in tqdm(range(len(m2))):
+    m2.loc[i,'匹配'] = m2.loc[i,'高区经理']+m2.loc[i,'区经姓名']
+
+people_1 = people.copy()
+
+a1 = people_1.loc[people_1['角色']== "区域经理助理"]
+a2 = people_1.loc[people_1['角色']== "区域经理"]
+a3 = people_1.loc[people_1['角色']== "APP功能试点,区域经理助理"]
+a4 = people_1.loc[people_1['角色']== "APP功能试点,区域经理"]
+
+people_1 = pd.concat([a1,a2,a3,a4])
+people_1 = people_1.reset_index(drop=True)
+
+people_1['匹配'] = ""
+
+for i in tqdm(range(len(people_1))):
+    try:
+        people_1.loc[i,'匹配'] = people_1.loc[i,'高区经理']+ people_1.loc[i,'姓名']
+    except:
+        pass
+
+people_1 = people_1[["匹配",'SA工号']]
+
+
+people_1 = people_1.rename(columns={'SA工号':"区经工号"})
+m2 = pd.merge(m2,people_1,on="匹配",how="left")
+
+df1 = m2.groupby(["城市经理","区经工号","区经姓名"],as_index=False)["首次M2单量","首次M2注册数"].sum()
+
+df1["首次M2逾期率"] = 0
+for i in tqdm(range(len(df1))):
+    df1.loc[i, "首次M2逾期率"] = df1.loc[i, "首次M2单量"] / df1.loc[i, "首次M2注册数"]
+
+df1["排名"] = df1["首次M2逾期率"].groupby(df1["城市经理"]).rank(ascending=1,method='first')
+count_of_daqu =  df1.groupby(["城市经理"],as_index=False)["区经工号"].count()
+
+df1 = pd.merge(df1,count_of_daqu,on="城市经理",how="left")
+
+for i in range(len(df1)):
+    if df1.loc[i,"排名"] <= round(df1.loc[i,"区经工号_y"]*0.6):
+        df1.loc[i,"是否免扣"] = "免扣"
+    else:
+        df1.loc[i,"是否免扣"] = "扣罚"
+
+df1.rename(columns = {'区经工号_x':'区经工号'})
+
+df1.to_excel("输出/区经区助扣罚免除.xlsx")
+
---
+++ 2-区域经理单笔扣罚.py
@@ -0,0 +1,86 @@
+
+"""
+
+绩效表是前推三个月和前推两个月的绩效数据
+销售人员清单： 当月31日/30日的
+首次M3要剔除 不在当期的单子
+
+"""
+
+
+import pandas as pd
+from tqdm import tqdm
+import numpy as np
+#导入数据
+M3 = pd.read_excel("首次M3明细/首次M3明细表.xlsx",dtype={'贷款编号':'O','SA工号':'O'})
+data1 = pd.read_excel("首次M3明细/绩效明细表.xlsx",dtype={'贷款编号':'O','核算工号':'O','区域经理助理编号':'O','区域经理编号':'O'})
+M3 = pd.merge(M3,data1,on="贷款编号",how="left")
+#导入销售人员清单
+people = pd.read_excel('首次M3明细/销售人员清单.xlsx',dtype={'工号':'O'})
+people = people[['工号','在岗状态']]
+#导入区经区助免除扣罚名单
+Free = pd.read_excel("输出/区经区助扣罚免除.xlsx",dtype={"区经工号_x":'O'})
+Free = Free[["区经工号_x","是否免扣"]]
+
+#选取字段
+M3=M3[['贷款编号','SA工号','SA姓名','产品名称','商户','门店','区域经理助理编号','区域经理助理姓名','区域经理编号','区域经理姓名']]
+
+#区域经理助理扣罚
+M3_qz = M3.copy()
+#提取区助
+for i in tqdm(range(len(M3_qz))):
+    if pd.isnull(M3_qz.loc[i,"区域经理助理编号"]) ==True:
+        M3_qz = M3_qz.drop(i)
+    else:
+        pass
+M3_qz = M3_qz.reset_index(drop=True)#重置索引
+
+people_qz = people.copy()#复制一份销售人员清单的在职状态
+people_qz = people_qz.rename(index=str,columns={"工号":'区域经理助理编号'})
+M3_qz = pd.merge(M3_qz,people_qz,on="区域经理助理编号",how="left")
+M3_qz = M3_qz[M3_qz["在岗状态"]=="在职"]#区助在职的人员
+M3_qz = M3_qz.reset_index(drop=True)#重置索引
+#扣罚逻辑
+for i in range(len(M3_qz)):
+    if M3_qz.loc[i,"产品名称"] == '003产品':
+        M3_qz.loc[i,"扣罚金额"] = 20
+    else:
+        M3_qz.loc[i,"扣罚金额"] = 18
+
+#区助是否免扣
+Free_qz = Free.copy()
+Free_qz = Free_qz.rename(index=str,columns={"区经工号_x":'区域经理助理编号'})
+M3_qz = pd.merge(M3_qz,Free_qz,on="区域经理助理编号",how="left")
+M3_qz = M3_qz[M3_qz["是否免扣"] =="扣罚"]
+
+#区域经理首次M3扣罚
+M3_qj = M3.copy()#复制一份
+people_qj = people.copy()#复制一份销售人员清单在职状态
+people_qj = people_qj.rename(index=str,columns={"工号":'区域经理编号'})#重命名
+for i in tqdm(range(len(M3_qj))):
+    if pd.isnull(M3_qj.loc[i,"区域经理编号"]) ==True:
+        M3_qj = M3_qj.drop(i)
+    else:
+        pass
+M3_qj = M3_qj.reset_index(drop=True)
+M3_qj = pd.merge(M3_qj,people_qj,on="区域经理编号",how="left")
+M3_qj = M3_qj[M3_qj["在岗状态"]=="在职"]
+M3_qj = M3_qj.reset_index(drop=True)
+
+for i in range(len(M3_qj)):
+    if M3_qj.loc[i,"产品名称"] == '003产品':
+        M3_qj.loc[i,"扣罚金额"] = 20
+    else:
+        M3_qj.loc[i,"扣罚金额"] = 30
+
+#区经是否免扣
+Free_qj = Free.copy()
+Free_qj = Free_qj.rename(index=str,columns={"区经工号_x":'区域经理编号'})
+M3_qj = pd.merge(M3_qj,Free_qj,on="区域经理编号",how="left")
+M3_qj = M3_qj[M3_qj["是否免扣"] =="扣罚"]
+
+#合并区经和区助的扣罚
+print("计算完成！正在保存文件")
+M3_result = pd.concat([M3_qj,M3_qz],sort=False)
+M3_result.to_excel("输出/区经区助单笔扣罚.xlsx")
+print("文件保存成功！good luck!")
